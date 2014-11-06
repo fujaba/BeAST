19d6f1b817683cbbd11ff75ade57a06618308e92
MoritzWilde <moritzw86@gmx.de>
2014-11-06 17:29:42 +0100

    added addFeatures

---
+++ de.uks.beast.editor/src/de/uks/beast/editor/features/AddRackFeature.java
@@ -0,0 +1,35 @@
+package de.uks.beast.editor.features;
+
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.IAddContext;
+import org.eclipse.graphiti.features.impl.AbstractAddFeature;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+
+public class AddRackFeature extends AbstractAddFeature
+{
+	
+	public AddRackFeature(IFeatureProvider fp)
+	{
+		super(fp);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	@Override
+	public boolean canAdd(IAddContext context)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+	
+	@Override
+	public PictogramElement add(IAddContext context)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
