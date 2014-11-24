26b1975fffb4abad635534c91a5cee9603cdc24b
kaaas <kassem_t@hotmail.de>
2014-11-24 10:55:02 +0100

    added flavor

--- server/src/main/java/de/uks/beast/server/akka/ReceiveActor.java
+++ server/src/main/java/de/uks/beast/server/akka/ReceiveActor.java
@@ -10,31 +10,31 @@ import de.uks.beast.server.BeastService;
 public class ReceiveActor extends UntypedActor {
 
 	private static Logger logger = LogManager.getLogger(ReceiveActor.class);
-	
+
 	private BeastService service;
-	
+
 	public ReceiveActor(BeastService service) {
 		this.service = service;
 	}
-	
+
 	@Override
 	public void onReceive(Object obj) throws Exception {
-		if (!service.getEnvironment().isAuthenticated()) {
-			service.getEnvironment().authenticate();
-		}
-		
-		if (service.getEnvironment().isAuthenticated()) {
-			if (obj instanceof Hardware) {
+		if (obj instanceof Hardware) {
+			if (!service.getEnvironment().isAuthenticated()) {
+				service.getEnvironment().authenticate();
+			}
+
+			if (service.getEnvironment().isAuthenticated()) {
 				logger.info("Received new hardware configuration");
 				service.getEnvironment().createHardwareDefiniton((Hardware) obj);
+				service.getEnvironment().startVirtualMachine();
 			} else {
-				unhandled(obj);
+				// handle unauthenticated failures
 			}
 		} else {
-			// handle unauthenticated failures
-			
+			unhandled(obj);
 		}
-		
+
 	}
 
 }
