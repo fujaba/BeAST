1dfc3b01ba8ed16e930b163f791bc2b1d46f651f
MoritzWilde <moritzw86@gmx.de>
2015-01-22 21:33:24 +0100

    added logger to BeASTAction

--- de.uks.beast.editor/src/de/uks/beast/editor/action/BeASTAction.java
+++ de.uks.beast.editor/src/de/uks/beast/editor/action/BeASTAction.java
@@ -1,5 +1,7 @@
 package de.uks.beast.editor.action;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -8,28 +10,41 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
-public class BeASTAction implements IObjectActionDelegate {
-
-	private IStructuredSelection selection;
-	private Shell shell;
-
+public class BeASTAction implements IObjectActionDelegate
+{
+	private static final Logger		LOG	= LogManager.getLogger(BeASTAction.class);
+	private IStructuredSelection	selection;
+	private Shell					shell;
+	
+	
+	
 	@Override
-	public void run(IAction action) {
-		IFile file = (IFile) selection.getFirstElement();
-		System.out.println(file);
+	public void run(final IAction action)
+	{
+		final IFile file = (IFile) selection.getFirstElement();
+		LOG.debug("IFile: " + file);
 	}
-
+	
+	
+	
 	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
+	public void selectionChanged(final IAction action, final ISelection selection)
+	{
 		this.selection = (IStructuredSelection) selection;
 	}
-
+	
+	
+	
 	@Override
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	public void setActivePart(final IAction action, final IWorkbenchPart targetPart)
+	{
 		shell = targetPart.getSite().getShell();
 	}
-
-	public Shell getShell() {
+	
+	
+	
+	public Shell getShell()
+	{
 		return shell;
 	}
 	
