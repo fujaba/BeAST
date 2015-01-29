a7bfdf4d0c76109ab5723b8540472448f1225e3e
kassem <kassem@se-ma-pc47.se.eecs.uni-kassel.de>
2015-01-29 13:40:39 +0100

    added kafka and some  buildpath fixes

---
+++ de.uks.beast.client/src/de/uks/beast/api/kafka/KafkaListener.java
@@ -0,0 +1,60 @@
+package de.uks.beast.api.kafka;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.message.MessageAndMetadata;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class KafkaListener {
+
+	private static Logger logger = LogManager.getLogger(KafkaListener.class);
+	
+	private ConsumerConnector consumer;
+	private String topic;
+
+	public KafkaListener(String zookeeper, String topic) {
+		String groupID = "de.uks.beast";
+		this.topic = topic;
+		
+		Properties props = new Properties();
+		props.put("zookeeper.connect", zookeeper);
+		props.put("group.id", groupID);
+		props.put("serializer.class", "com.ottogroup.bi.splittest.channel.EventSerializer");
+		props.put("zk.sessiontimeout.ms", "400");
+		props.put("zk.synctime.ms", "200");
+		this.consumer = Consumer.createJavaConsumerConnector(new ConsumerConfig(props));
+	}
+
+	public void pollChannel() {
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		topicCountMap.put(topic, new Integer(1));
+		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer
+				.createMessageStreams(topicCountMap);
+		KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
+		ConsumerIterator<byte[], byte[]> it = stream.iterator();
+		while (it.hasNext()) {
+			readMessage(it);
+		}
+	}
+
+	private String readMessage(ConsumerIterator<byte[], byte[]> it) {
+		MessageAndMetadata<byte[], byte[]> next = it.next();
+		try {
+			return new String(next.message(), "UTF-8");
+		} catch (Exception e) {
+			logger.error("Could not parse event", e);
+			System.exit(0);
+		}
+		return null;
+	}
+}
