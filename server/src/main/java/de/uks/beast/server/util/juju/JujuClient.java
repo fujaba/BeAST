8aa3e492e78a756a59038ebbb2ddecd2cca4f83d
Valentyn Kukushkin <unik@users.noreply.github.com>
2015-03-03 12:56:12 +0100

    back to proper parameter naming

--- server/src/main/java/de/uks/beast/server/util/juju/JujuClient.java
+++ server/src/main/java/de/uks/beast/server/util/juju/JujuClient.java
@@ -1,29 +1,26 @@
 package de.uks.beast.server.util.juju;
 
-import java.io.IOException;
-
 public class JujuClient {
-	
+
     /**
      * Deploy a service.
      *
      * @param servicename
      * @return
-     * @throws IOException
      */
     public static RuntimeOutput deploy(String servicename) {
-    	 return RuntimeExec.run("juju deploy " + servicename);
+        return RuntimeExec.run("juju deploy " + servicename);
     }
 
     /**
-     * Deploy a service with specified subservice.
+     * Deploy a service with specified alias.
+     *
      * @param servicename
      * @param alias is optional (eg. juju deploy hadoop hadoop-master)
      * @return
-     * @throws IOException
      */
-    public static RuntimeOutput deploy(String servicename, String subservice) {
-        return RuntimeExec.run("juju deploy " + servicename + " " + subservice);
+    public static RuntimeOutput deploy(String servicename, String alias) {
+        return RuntimeExec.run("juju deploy " + servicename + " " + alias);
     }
 
     /**
@@ -33,26 +30,26 @@ public class JujuClient {
      * @param alias is optional (eg. juju deploy hadoop hadoop-master)
      * @param machineNumber
      * @return
-     * @throws IOException
      */
-    public static RuntimeOutput deploy(String servicename, String subservice, int machineNumber) throws IOException {
-        return RuntimeExec.run("juju deploy " + servicename + " " + subservice + " --to " + machineNumber);
+    public static RuntimeOutput deploy(String servicename, String alias, int machineNumber) {
+        return RuntimeExec.run("juju deploy " + servicename + " " + alias + " --to " + machineNumber);
     }
-    
+
     /**
      * Add manual provisioned machine
+     *
      * @param userAtHost e.g. [user@]host
      * @return # of the added machine
-     * @throws IOException
-     * @info http://askubuntu.com/questions/469618/juju-deployment-error-on-manually-provisioned-machine
+     * @info
+     * http://askubuntu.com/questions/469618/juju-deployment-error-on-manually-provisioned-machine
      */
     public static int addMachine(String userAtHost) {
-		final RuntimeOutput out = RuntimeExec.run("juju add-machine ssh:" + userAtHost);
-		String message = out.getMessage();
-		if (message.contains("created machine")) {
+        final RuntimeOutput out = RuntimeExec.run("juju add-machine ssh:" + userAtHost);
+        String message = out.getMessage();
+        if (message.contains("created machine")) {
             return Integer.parseInt(message.replaceAll("[^\\d.]", ""));
         }
         return -1;
-	}
+    }
 
 }
