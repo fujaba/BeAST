a6e42f4de221488964f442b9d22c3921fdd9bc3e
Valentyn Kukushkin <unik@users.noreply.github.com>
2015-05-19 11:16:32 +0200

    file upload resource (REST)

--- hadoop-service/hds-server/src/main/java/de/uks/beast/hds/server/resources/FileUpload.java
+++ hadoop-service/hds-server/src/main/java/de/uks/beast/hds/server/resources/FileUpload.java
@@ -8,9 +8,11 @@ package de.uks.beast.hds.server.resources;
 import org.glassfish.jersey.media.multipart.*;
 
 import java.io.InputStream;
+import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -18,28 +20,36 @@ import javax.ws.rs.core.Response;
  *
  * @author valentyn
  */
+
 @Path("files")
 public class FileUpload {
-    
+
     // root directory for file upload.
     // each job should eventualy have a subfolder here.
     private static final String FILES_ROOT_DIR = ""; // TODO add path
-    
+
     // • Send the file first in a multipart/form-data POST, and return an ID to the client. 
     // The client then sends the metadata with the ID, and the server re-associates the file and the metadata.
     // or
     // • Send the metadata first, and return an ID to the client. 
     // The client then sends the file with the ID, and the server re-associates the file and the metadata.
-    
-//    @POST
-////    @Path("/upload")
-//    @Consumes(MediaType.MULTIPART_FORM_DATA)
-//    public Response fileUpload(
-//            @FormDataParam("file") InputStream fis,
-//            @FormDataParam("file") FormDataContentDisposition fileDetails,
-//            @FormDataParam("path") String path) {
-//        
-//        return Response.status(Response.Status.OK).entity("File uploaded.").build();
-//    }
-    
+    @POST @Path("upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response fileUpload(
+            @FormDataParam("file") InputStream fis,
+            @FormDataParam("file") FormDataContentDisposition fileDetails,
+            @FormDataParam("path") String path) {
+        /*
+        List<FormDataBodyPart> parts = formParams.getFields("file");
+        for (FormDataBodyPart part : parts) {
+            FormDataContentDisposition file = part.getFormDataContentDisposition();
+        }
+        */
+        
+        // TODO saveToDisk();
+        
+        return Response.status(Response.Status.OK).entity("File uploaded.").build();
+    }
+
 }
