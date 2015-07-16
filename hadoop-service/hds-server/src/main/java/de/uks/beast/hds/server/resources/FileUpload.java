a7ffc3010dce6eb0e9f06ad280d5ac47b038fa7a
Valentyn Kukushkin <unik@users.noreply.github.com>
2015-07-16 15:17:40 +0200

    file upload + test

--- hadoop-service/hds-server/src/main/java/de/uks/beast/hds/server/resources/FileUpload.java
+++ hadoop-service/hds-server/src/main/java/de/uks/beast/hds/server/resources/FileUpload.java
@@ -5,51 +5,74 @@
  */
 package de.uks.beast.hds.server.resources;
 
-import org.glassfish.jersey.media.multipart.*;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
-import java.io.InputStream;
-import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.*;
 
 /**
  *
  * @author valentyn
  */
 
-@Path("files")
+@Path("fileupload")
 public class FileUpload {
 
     // root directory for file upload.
     // each job should eventualy have a subfolder here.
-    private static final String FILES_ROOT_DIR = ""; // TODO add path
+    // TODO set root directory for the zip file
+    private static final String TARGET_FILES_ROOT_DIR = "/tmp/";
 
     // • Send the file first in a multipart/form-data POST, and return an ID to the client. 
     // The client then sends the metadata with the ID, and the server re-associates the file and the metadata.
     // or
     // • Send the metadata first, and return an ID to the client. 
     // The client then sends the file with the ID, and the server re-associates the file and the metadata.
-    @POST @Path("upload")
+    // http://localhost:8080/myapp/files/upload
+
+    @POST /*@Path("upload")*/
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
     public Response fileUpload(
             @FormDataParam("file") InputStream fis,
             @FormDataParam("file") FormDataContentDisposition fileDetails,
             @FormDataParam("path") String path) {
-        /*
-        List<FormDataBodyPart> parts = formParams.getFields("file");
-        for (FormDataBodyPart part : parts) {
-            FormDataContentDisposition file = part.getFormDataContentDisposition();
-        }
-        */
+
+        final String destinationFileLocation = TARGET_FILES_ROOT_DIR + fileDetails.getFileName();
+//        System.out.println("**** [path]: " + path);
+
+        System.out.println("**** [DESTINATION]: " + fileDetails.getFileName());
         
-        // TODO saveToDisk();
+        // save received file to local storage
+        saveToDisk(fis, destinationFileLocation);
+
+        // TODO unzip file to specified targets
         
-        return Response.status(Response.Status.OK).entity("File uploaded.").build();
+        return Response.status(Response.Status.OK).entity("File uploaded to: " + destinationFileLocation).build();
     }
 
+    private void saveToDisk(InputStream uploadedInputStream, String uploadedFileLocation) {
+
+        try {
+            OutputStream out; // = new FileOutputStream(new File(uploadedFileLocation));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            out = new FileOutputStream(new File(uploadedFileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- hadoop-service/hds-server/src/test/java/de/uks/beast/hds/server/JobsTest.java
+++ hadoop-service/hds-server/src/test/java/de/uks/beast/hds/server/JobsTest.java
@@ -2,38 +2,52 @@ package de.uks.beast.hds.server;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import de.uks.beast.hds.server.resources.FileUpload;
 import org.glassfish.grizzly.http.server.HttpServer;
 
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.media.multipart.*;
+import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+
 import static org.junit.Assert.assertEquals;
 
-public class JobsTest {
+public class JobsTest extends JerseyTest {
 
     private HttpServer server;
     private WebTarget target;
 
-    @Before
-    public void setUp() throws Exception {
+    @Before public void setUp() throws Exception {
         // start the server
         server = Main.startServer();
         // create the client
-        Client c = ClientBuilder.newClient();
+//        Client client = ClientBuilder.newClient();
+        Client client = ClientBuilder.newBuilder().register(MultiPartFeature.class).build();
 
         // uncomment the following line if you want to enable
         // support for JSON in the client (you also have to uncomment
         // dependency on jersey-media-json module in pom.xml and Main.startServer())
         // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
+        // client.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
 
-        target = c.target(Main.BASE_URI);
+        target = client.target(Main.BASE_URI);
+        System.out.println("**** target uri: " + target.getUri());
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @After public void tearDown() throws Exception {
         server.stop();
     }
 
@@ -44,5 +58,43 @@ public class JobsTest {
     public void testGetIt() {
         String responseMsg = target.path("jobs").request().get(String.class);
         assertEquals("Got it!", responseMsg);
+    }
+
+
+    @Override protected Application configure() {
+        return new ResourceConfig(FileUpload.class);
+    }
+
+    @Override protected void configureClient(ClientConfig clientConfig) {
+        clientConfig.register(MultiPartFeature.class);
+    }
+
+
+    @Test public void testFileUpload() {
+        System.out.println("**** Start file upload");
+
+        // source file
+        String FILE = "/BeAST-20140122.zip";
+        URL url = this.getClass().getResource(FILE);
+        File data = new File(url.getFile());
+
+        // source file exists
+        System.out.println("**** [source file exists]: " + data.exists());
+
+        MultiPart multiPart = new MultiPart();
+        multiPart.setMediaType(MediaType.MULTIPART_FORM_DATA_TYPE);
+
+        FileDataBodyPart fileDataBodyPart = new FileDataBodyPart("file", data, MediaType.APPLICATION_OCTET_STREAM_TYPE);
+        multiPart.bodyPart(fileDataBodyPart);
+//        fileDataBodyPart.setContentDisposition(FormDataContentDisposition.name("file.zip").fileName(FILE).build());
+
+
+        Response response = target.path("fileupload").request().post(
+                Entity.entity(multiPart, multiPart.getMediaType()));
+
+        System.out.println("**** upload response status: " + response.getStatus());
+        System.out.println("**** " + response. getStatusInfo() + " " + response);
+
+        response.close();
     }
 }
