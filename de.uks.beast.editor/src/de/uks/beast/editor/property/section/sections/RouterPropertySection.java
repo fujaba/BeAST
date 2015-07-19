d0e064f8edcb317c6ccdace6ef37bd8416aeb53a
MoritzWilde <moritzw86@gmx.de>
2015-07-19 20:54:34 +0200

    refactored router property view

--- de.uks.beast.editor/src/de/uks/beast/editor/property/section/sections/RouterPropertySection.java
+++ de.uks.beast.editor/src/de/uks/beast/editor/property/section/sections/RouterPropertySection.java
@@ -1,9 +1,5 @@
 package de.uks.beast.editor.property.section.sections;
 
-import static de.uks.beast.editor.util.Properties.EXTERNAL_GATEWAY_LABEL;
-import static de.uks.beast.editor.util.Properties.ID_LABEL;
-import static de.uks.beast.editor.util.Properties.IP_LABEL;
-import static de.uks.beast.editor.util.Properties.TRANSFER;
 import model.Router;
 
 import org.apache.log4j.LogManager;
@@ -14,29 +10,19 @@ import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.platform.GFPropertySection;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
 
-import de.uks.beast.editor.util.Dimensions;
-import de.uks.beast.editor.util.ToolTips;
+import de.uks.beast.editor.property.section.views.RouterPropertyView;
 
 public class RouterPropertySection extends GFPropertySection implements ITabbedPropertyConstants
 {
 	private static final Logger			LOG	= LogManager.getLogger(RouterPropertySection.class);
-	private Text						ipTextFld;
-	private Text						externalGatewayTextFld;
-	private Text						idTextFld;
-	private Button						submitBtn;
+	private RouterPropertyView			routerPropertyView;
 	private Router						router;
 	private TransactionalEditingDomain	domain;
 	
@@ -49,65 +35,17 @@ public class RouterPropertySection extends GFPropertySection implements ITabbedP
 		
 		final TabbedPropertySheetWidgetFactory factory = getWidgetFactory();
 		final Composite composite = factory.createFlatFormComposite(parent);
-		FormData data;
 		
-		//Property_ip
-		ipTextFld = factory.createText(composite, "");
-		data = new FormData();
-		data.left = new FormAttachment(0, Dimensions.ROUTER_PROP_LABEL_WIDTH);
-		data.right = new FormAttachment(100, 0);
-		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE);
-		ipTextFld.setToolTipText(ToolTips.IP_PROP_TIP.get());
-		ipTextFld.setLayoutData(data);
+		routerPropertyView = new RouterPropertyView(composite, factory);
 		
-		final CLabel valueLabel = factory.createCLabel(composite, IP_LABEL.get());
-		data = new FormData();
-		data.left = new FormAttachment(0, 0);
-		data.right = new FormAttachment(ipTextFld, valueLabel.getText().length());
-		data.top = new FormAttachment(ipTextFld, 0, SWT.CENTER);
-		valueLabel.setLayoutData(data);
-		
-		//Property_cpuType
-		externalGatewayTextFld = factory.createText(composite, "");
-		data = new FormData();
-		data.left = new FormAttachment(0, Dimensions.ROUTER_PROP_LABEL_WIDTH);
-		data.right = new FormAttachment(100, 0);
-		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 25);
-		externalGatewayTextFld.setToolTipText(ToolTips.EXT_GATEWAY_PROP_TIP.get());
-		externalGatewayTextFld.setLayoutData(data);
-		
-		final CLabel valueLabe3 = factory.createCLabel(composite, EXTERNAL_GATEWAY_LABEL.get());
-		data = new FormData();
-		data.left = new FormAttachment(0, 0);
-		data.right = new FormAttachment(externalGatewayTextFld, valueLabe3.getText().length());
-		data.top = new FormAttachment(externalGatewayTextFld, 0, SWT.CENTER);
-		valueLabe3.setLayoutData(data);
-		
-		//Property_diskSpace
-		idTextFld = factory.createText(composite, "");
-		data = new FormData();
-		data.left = new FormAttachment(0, Dimensions.ROUTER_PROP_LABEL_WIDTH);
-		data.right = new FormAttachment(100, 0);
-		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 50);
-		idTextFld.setToolTipText(ToolTips.ID_PROP_TIP.get());
-		idTextFld.setLayoutData(data);
-		
-		final CLabel valueLabe5 = factory.createCLabel(composite, ID_LABEL.get());
-		data = new FormData();
-		data.left = new FormAttachment(0, 0);
-		data.right = new FormAttachment(idTextFld, valueLabe5.getText().length());
-		data.top = new FormAttachment(idTextFld, 0, SWT.CENTER);
-		valueLabe5.setLayoutData(data);
-		
-		//Property_submit
-		submitBtn = factory.createButton(composite, TRANSFER.get(), 0);
-		data = new FormData();
-		data.left = new FormAttachment(0, 20);
-		data.right = new FormAttachment(20, 0);
-		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 125);
-		submitBtn.setLayoutData(data);
-		submitBtn.setToolTipText(ToolTips.TRANSFER_BTN_TIP.get());
-		submitBtn.addSelectionListener(new SelectionListener() {
+		addListener();
+	}
+	
+	
+	
+	private void addListener()
+	{
+		routerPropertyView.setListenerToSubmitBtn(new SelectionListener() {
 			
 			@Override
 			public void widgetSelected(SelectionEvent arg0)
@@ -115,9 +53,9 @@ public class RouterPropertySection extends GFPropertySection implements ITabbedP
 				domain.getCommandStack().execute(new RecordingCommand(domain) {
 					public void doExecute()
 					{
-						router.setIp(ipTextFld.getText());
-						router.setExternalGateway(externalGatewayTextFld.getText());
-						router.setId(idTextFld.getText());
+						router.setIp(routerPropertyView.getIpInput());
+						router.setExternalGateway(routerPropertyView.getExternalGatewayInput());
+						router.setId(routerPropertyView.getIdInput());
 						
 						LOG.debug("router hash: " + router.hashCode() + " -> ip: " + router.getIp());
 						LOG.debug("router hash: " + router.hashCode() + " -> external gateway: " + router.getExternalGateway());
@@ -133,7 +71,7 @@ public class RouterPropertySection extends GFPropertySection implements ITabbedP
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0)
 			{
-				// TODO Auto-generated method stub
+				// Nothing to do!
 				
 			}
 		});
@@ -143,9 +81,9 @@ public class RouterPropertySection extends GFPropertySection implements ITabbedP
 	
 	private void setPreDefinedValuesToSheet()
 	{
-		ipTextFld.setText(router.getIp());
-		externalGatewayTextFld.setText("" + router.getExternalGateway());
-		idTextFld.setText(router.getId());
+		routerPropertyView.setIpInput(router.getIp());
+		routerPropertyView.setExternalGatewayInput("" + router.getExternalGateway());
+		routerPropertyView.setIdInput(router.getId());
 	}
 	
 	
--- de.uks.beast.editor/src/de/uks/beast/editor/property/section/views/RouterPropertyView.java
+++ de.uks.beast.editor/src/de/uks/beast/editor/property/section/views/RouterPropertyView.java
@@ -1,6 +1,153 @@
 package de.uks.beast.editor.property.section.views;
 
+import static de.uks.beast.editor.util.Properties.EXTERNAL_GATEWAY_LABEL;
+import static de.uks.beast.editor.util.Properties.ID_LABEL;
+import static de.uks.beast.editor.util.Properties.IP_LABEL;
+import static de.uks.beast.editor.util.Properties.TRANSFER;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
+
+import de.uks.beast.editor.util.Dimensions;
+import de.uks.beast.editor.util.ToolTips;
+
 public class RouterPropertyView
 {
+	private Text	ipTextFld;
+	private Text	externalGatewayTextFld;
+	private Text	idTextFld;
+	private Button	submitBtn;
+	
+	
+	
+	public RouterPropertyView(final Composite parent, final TabbedPropertySheetWidgetFactory factory)
+	{
+		createContent(parent, factory);
+	}
+	
+	
+	
+	private void createContent(final Composite composite, final TabbedPropertySheetWidgetFactory factory)
+	{
+		FormData data;
+		
+		ipTextFld = factory.createText(composite, "");
+		data = new FormData();
+		data.left = new FormAttachment(0, Dimensions.ROUTER_PROP_LABEL_WIDTH);
+		data.right = new FormAttachment(100, 0);
+		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE);
+		ipTextFld.setToolTipText(ToolTips.IP_PROP_TIP.get());
+		ipTextFld.setLayoutData(data);
+		
+		final CLabel valueLabel = factory.createCLabel(composite, IP_LABEL.get());
+		data = new FormData();
+		data.left = new FormAttachment(0, 0);
+		data.right = new FormAttachment(ipTextFld, valueLabel.getText().length());
+		data.top = new FormAttachment(ipTextFld, 0, SWT.CENTER);
+		valueLabel.setLayoutData(data);
+		
+		externalGatewayTextFld = factory.createText(composite, "");
+		data = new FormData();
+		data.left = new FormAttachment(0, Dimensions.ROUTER_PROP_LABEL_WIDTH);
+		data.right = new FormAttachment(100, 0);
+		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 25);
+		externalGatewayTextFld.setToolTipText(ToolTips.EXT_GATEWAY_PROP_TIP.get());
+		externalGatewayTextFld.setLayoutData(data);
+		
+		final CLabel valueLabe3 = factory.createCLabel(composite, EXTERNAL_GATEWAY_LABEL.get());
+		data = new FormData();
+		data.left = new FormAttachment(0, 0);
+		data.right = new FormAttachment(externalGatewayTextFld, valueLabe3.getText().length());
+		data.top = new FormAttachment(externalGatewayTextFld, 0, SWT.CENTER);
+		valueLabe3.setLayoutData(data);
+		
+		idTextFld = factory.createText(composite, "");
+		data = new FormData();
+		data.left = new FormAttachment(0, Dimensions.ROUTER_PROP_LABEL_WIDTH);
+		data.right = new FormAttachment(100, 0);
+		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 50);
+		idTextFld.setToolTipText(ToolTips.ID_PROP_TIP.get());
+		idTextFld.setLayoutData(data);
+		
+		final CLabel valueLabe5 = factory.createCLabel(composite, ID_LABEL.get());
+		data = new FormData();
+		data.left = new FormAttachment(0, 0);
+		data.right = new FormAttachment(idTextFld, valueLabe5.getText().length());
+		data.top = new FormAttachment(idTextFld, 0, SWT.CENTER);
+		valueLabe5.setLayoutData(data);
+		
+		submitBtn = factory.createButton(composite, TRANSFER.get(), 0);
+		data = new FormData();
+		data.left = new FormAttachment(0, 20);
+		data.right = new FormAttachment(20, 0);
+		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 125);
+		submitBtn.setLayoutData(data);
+		submitBtn.setToolTipText(ToolTips.TRANSFER_BTN_TIP.get());
+	}
+	
+	
+	
+	public void setListenerToSubmitBtn(final SelectionListener listener)
+	{
+		submitBtn.addSelectionListener(listener);
+	}
+	
+	
+	
+	public String getIpInput()
+	{
+		return ipTextFld.getText();
+	}
+	
+	
+	
+	public String getIdInput()
+	{
+		return idTextFld.getText();
+	}
+	
+	
+	
+	public String getExternalGatewayInput()
+	{
+		return externalGatewayTextFld.getText();
+	}
+	
+	
+	
+	public void setIpInput(final String input)
+	{
+		if (input != null && !input.isEmpty())
+		{
+			ipTextFld.setText(input);
+		}
+	}
+	
+	
+	
+	public void setIdInput(final String input)
+	{
+		if (input != null && !input.isEmpty())
+		{
+			idTextFld.setText(input);
+		}
+	}
+	
+	
+	
+	public void setExternalGatewayInput(final String input)
+	{
+		if (input != null && !input.isEmpty())
+		{
+			externalGatewayTextFld.setText(input);
+		}
+	}
 	
 }
