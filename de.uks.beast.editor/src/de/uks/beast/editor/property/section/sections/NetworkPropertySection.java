3260b2680155ed4ab07ac1f0be5a5510b79b9515
MoritzWilde <moritzw86@gmx.de>
2015-07-19 20:45:41 +0200

    refactored network property view

--- de.uks.beast.editor/src/de/uks/beast/editor/property/section/sections/NetworkPropertySection.java
+++ de.uks.beast.editor/src/de/uks/beast/editor/property/section/sections/NetworkPropertySection.java
@@ -1,10 +1,5 @@
 package de.uks.beast.editor.property.section.sections;
 
-import static de.uks.beast.editor.util.Properties.DNS_LABEL;
-import static de.uks.beast.editor.util.Properties.GATEWAY_LABEL;
-import static de.uks.beast.editor.util.Properties.IP_LABEL;
-import static de.uks.beast.editor.util.Properties.SUBNET_MASK_LABEL;
-import static de.uks.beast.editor.util.Properties.TRANSFER;
 import model.Network;
 
 import org.apache.log4j.LogManager;
@@ -15,32 +10,21 @@ import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.platform.GFPropertySection;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
 
-import de.uks.beast.editor.util.Dimensions;
-import de.uks.beast.editor.util.ToolTips;
+import de.uks.beast.editor.property.section.views.NetworkPropertyView;
 
 public class NetworkPropertySection extends GFPropertySection implements ITabbedPropertyConstants
 {
 	private static final Logger			LOG	= LogManager.getLogger(NetworkPropertySection.class);
-	private Text						ipTextFld;
-	private Text						subnetTextFld;
-	private Text						gatewayTextFld;
-	private Text						dnsTextFld;
-	private Button						submitBtn;
 	private Network						network;
 	private TransactionalEditingDomain	domain;
+	private NetworkPropertyView			networkPropertyView;
 	
 	
 	
@@ -51,81 +35,17 @@ public class NetworkPropertySection extends GFPropertySection implements ITabbed
 		
 		final TabbedPropertySheetWidgetFactory factory = getWidgetFactory();
 		final Composite composite = factory.createFlatFormComposite(parent);
-		FormData data;
 		
-		//Property_ip
-		ipTextFld = factory.createText(composite, "");
-		data = new FormData();
-		data.left = new FormAttachment(0, Dimensions.NETWORK_PROP_LABEL_WIDTH);
-		data.right = new FormAttachment(100, 0);
-		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE);
-		ipTextFld.setToolTipText(ToolTips.IP_PROP_TIP.get());
-		ipTextFld.setLayoutData(data);
+		networkPropertyView = new NetworkPropertyView(composite, factory);
 		
-		final CLabel valueLabel = factory.createCLabel(composite, IP_LABEL.get());
-		data = new FormData();
-		data.left = new FormAttachment(0, 0);
-		data.right = new FormAttachment(ipTextFld, valueLabel.getText().length());
-		data.top = new FormAttachment(ipTextFld, 0, SWT.CENTER);
-		valueLabel.setLayoutData(data);
-		
-		//Property_cpuAmount
-		subnetTextFld = factory.createText(composite, "");
-		data = new FormData();
-		data.left = new FormAttachment(0, Dimensions.NETWORK_PROP_LABEL_WIDTH);
-		data.right = new FormAttachment(100, 0);
-		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 25);
-		subnetTextFld.setToolTipText(ToolTips.SUBNET_MASK_PROP_TIP.get());
-		subnetTextFld.setLayoutData(data);
-		
-		final CLabel valueLabe2 = factory.createCLabel(composite, SUBNET_MASK_LABEL.get());
-		data = new FormData();
-		data.left = new FormAttachment(0, 0);
-		data.right = new FormAttachment(subnetTextFld, valueLabe2.getText().length());
-		data.top = new FormAttachment(subnetTextFld, 0, SWT.CENTER);
-		valueLabe2.setLayoutData(data);
-		
-		//Property_cpuType
-		gatewayTextFld = factory.createText(composite, "");
-		data = new FormData();
-		data.left = new FormAttachment(0, Dimensions.NETWORK_PROP_LABEL_WIDTH);
-		data.right = new FormAttachment(100, 0);
-		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 50);
-		gatewayTextFld.setToolTipText(ToolTips.GATEWAY_PROP_TIP.get());
-		gatewayTextFld.setLayoutData(data);
-		
-		final CLabel valueLabe3 = factory.createCLabel(composite, GATEWAY_LABEL.get());
-		data = new FormData();
-		data.left = new FormAttachment(0, 0);
-		data.right = new FormAttachment(gatewayTextFld, valueLabe3.getText().length());
-		data.top = new FormAttachment(gatewayTextFld, 0, SWT.CENTER);
-		valueLabe3.setLayoutData(data);
-		
-		//Property_ram
-		dnsTextFld = factory.createText(composite, "");
-		data = new FormData();
-		data.left = new FormAttachment(0, Dimensions.NETWORK_PROP_LABEL_WIDTH);
-		data.right = new FormAttachment(100, 0);
-		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 75);
-		dnsTextFld.setToolTipText(ToolTips.DNS_PROP_TIP.get());
-		dnsTextFld.setLayoutData(data);
-		
-		final CLabel valueLabe4 = factory.createCLabel(composite, DNS_LABEL.get());
-		data = new FormData();
-		data.left = new FormAttachment(0, 0);
-		data.right = new FormAttachment(dnsTextFld, valueLabe4.getText().length());
-		data.top = new FormAttachment(dnsTextFld, 0, SWT.CENTER);
-		valueLabe4.setLayoutData(data);
-		
-		//Property_submit
-		submitBtn = factory.createButton(composite, TRANSFER.get(), 0);
-		data = new FormData();
-		data.left = new FormAttachment(0, 20);
-		data.right = new FormAttachment(20, 0);
-		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 125);
-		submitBtn.setLayoutData(data);
-		submitBtn.setToolTipText(ToolTips.TRANSFER_BTN_TIP.get());
-		submitBtn.addSelectionListener(new SelectionListener() {
+		addListener();
+	}
+	
+	
+	
+	private void addListener()
+	{
+		networkPropertyView.setListenerToSubmitBtn(new SelectionListener() {
 			
 			@Override
 			public void widgetSelected(SelectionEvent arg0)
@@ -133,10 +53,10 @@ public class NetworkPropertySection extends GFPropertySection implements ITabbed
 				domain.getCommandStack().execute(new RecordingCommand(domain) {
 					public void doExecute()
 					{
-						network.setIp(ipTextFld.getText());
-						network.setSubnetmask(subnetTextFld.getText());
-						network.setGateway(gatewayTextFld.getText());
-						network.setDns(dnsTextFld.getText());
+						network.setIp(networkPropertyView.getIpInput());
+						network.setSubnetmask(networkPropertyView.getSubnetInput());
+						network.setGateway(networkPropertyView.getGatewayInput());
+						network.setDns(networkPropertyView.getDnsInput());
 						
 						LOG.debug("network hash: " + network.hashCode() + " -> ip: " + network.getIp());
 						LOG.debug("network hash: " + network.hashCode() + " -> subnet: " + network.getSubnetmask());
@@ -153,7 +73,7 @@ public class NetworkPropertySection extends GFPropertySection implements ITabbed
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0)
 			{
-				// TODO Auto-generated method stub
+				// Nothing to do!
 				
 			}
 		});
@@ -163,10 +83,10 @@ public class NetworkPropertySection extends GFPropertySection implements ITabbed
 	
 	private void setPreDefinedValuesToSheet()
 	{
-		ipTextFld.setText(network.getIp());
-		subnetTextFld.setText("" + network.getSubnetmask());
-		gatewayTextFld.setText(network.getGateway());
-		dnsTextFld.setText("" + network.getDns());
+		networkPropertyView.setIpInput(network.getIp());
+		networkPropertyView.setSubnetInput("" + network.getSubnetmask());
+		networkPropertyView.setGatewayInput(network.getGateway());
+		networkPropertyView.setDnsInput("" + network.getDns());
 	}
 	
 	
--- de.uks.beast.editor/src/de/uks/beast/editor/property/section/views/NetworkPropertyView.java
+++ de.uks.beast.editor/src/de/uks/beast/editor/property/section/views/NetworkPropertyView.java
@@ -1,6 +1,191 @@
 package de.uks.beast.editor.property.section.views;
 
+import static de.uks.beast.editor.util.Properties.DNS_LABEL;
+import static de.uks.beast.editor.util.Properties.GATEWAY_LABEL;
+import static de.uks.beast.editor.util.Properties.IP_LABEL;
+import static de.uks.beast.editor.util.Properties.SUBNET_MASK_LABEL;
+import static de.uks.beast.editor.util.Properties.TRANSFER;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
+
+import de.uks.beast.editor.util.Dimensions;
+import de.uks.beast.editor.util.ToolTips;
+
 public class NetworkPropertyView
 {
+	private Text	ipTextFld;
+	private Text	subnetTextFld;
+	private Text	gatewayTextFld;
+	private Text	dnsTextFld;
+	private Button	submitBtn;
 	
+	
+	
+	public NetworkPropertyView(final Composite parent, final TabbedPropertySheetWidgetFactory factory)
+	{
+		createContent(parent, factory);
+	}
+	
+	
+	
+	private void createContent(final Composite composite, final TabbedPropertySheetWidgetFactory factory)
+	{
+		FormData data;
+		
+		//Property_ip
+		ipTextFld = factory.createText(composite, "");
+		data = new FormData();
+		data.left = new FormAttachment(0, Dimensions.NETWORK_PROP_LABEL_WIDTH);
+		data.right = new FormAttachment(100, 0);
+		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE);
+		ipTextFld.setToolTipText(ToolTips.IP_PROP_TIP.get());
+		ipTextFld.setLayoutData(data);
+		
+		final CLabel valueLabel = factory.createCLabel(composite, IP_LABEL.get());
+		data = new FormData();
+		data.left = new FormAttachment(0, 0);
+		data.right = new FormAttachment(ipTextFld, valueLabel.getText().length());
+		data.top = new FormAttachment(ipTextFld, 0, SWT.CENTER);
+		valueLabel.setLayoutData(data);
+		
+		//Property_cpuAmount
+		subnetTextFld = factory.createText(composite, "");
+		data = new FormData();
+		data.left = new FormAttachment(0, Dimensions.NETWORK_PROP_LABEL_WIDTH);
+		data.right = new FormAttachment(100, 0);
+		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 25);
+		subnetTextFld.setToolTipText(ToolTips.SUBNET_MASK_PROP_TIP.get());
+		subnetTextFld.setLayoutData(data);
+		
+		final CLabel valueLabe2 = factory.createCLabel(composite, SUBNET_MASK_LABEL.get());
+		data = new FormData();
+		data.left = new FormAttachment(0, 0);
+		data.right = new FormAttachment(subnetTextFld, valueLabe2.getText().length());
+		data.top = new FormAttachment(subnetTextFld, 0, SWT.CENTER);
+		valueLabe2.setLayoutData(data);
+		
+		//Property_cpuType
+		gatewayTextFld = factory.createText(composite, "");
+		data = new FormData();
+		data.left = new FormAttachment(0, Dimensions.NETWORK_PROP_LABEL_WIDTH);
+		data.right = new FormAttachment(100, 0);
+		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 50);
+		gatewayTextFld.setToolTipText(ToolTips.GATEWAY_PROP_TIP.get());
+		gatewayTextFld.setLayoutData(data);
+		
+		final CLabel valueLabe3 = factory.createCLabel(composite, GATEWAY_LABEL.get());
+		data = new FormData();
+		data.left = new FormAttachment(0, 0);
+		data.right = new FormAttachment(gatewayTextFld, valueLabe3.getText().length());
+		data.top = new FormAttachment(gatewayTextFld, 0, SWT.CENTER);
+		valueLabe3.setLayoutData(data);
+		
+		//Property_ram
+		dnsTextFld = factory.createText(composite, "");
+		data = new FormData();
+		data.left = new FormAttachment(0, Dimensions.NETWORK_PROP_LABEL_WIDTH);
+		data.right = new FormAttachment(100, 0);
+		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 75);
+		dnsTextFld.setToolTipText(ToolTips.DNS_PROP_TIP.get());
+		dnsTextFld.setLayoutData(data);
+		
+		final CLabel valueLabe4 = factory.createCLabel(composite, DNS_LABEL.get());
+		data = new FormData();
+		data.left = new FormAttachment(0, 0);
+		data.right = new FormAttachment(dnsTextFld, valueLabe4.getText().length());
+		data.top = new FormAttachment(dnsTextFld, 0, SWT.CENTER);
+		valueLabe4.setLayoutData(data);
+		
+		//Property_submit
+		submitBtn = factory.createButton(composite, TRANSFER.get(), 0);
+		data = new FormData();
+		data.left = new FormAttachment(0, 20);
+		data.right = new FormAttachment(20, 0);
+		data.top = new FormAttachment(0, Dimensions.PROP_LINE_VSPACE + 125);
+		submitBtn.setLayoutData(data);
+		submitBtn.setToolTipText(ToolTips.TRANSFER_BTN_TIP.get());
+	}
+	
+	
+	
+	public void setListenerToSubmitBtn(final SelectionListener listener)
+	{
+		submitBtn.addSelectionListener(listener);
+	}
+	
+	
+	
+	public String getIpInput()
+	{
+		return ipTextFld.getText();
+	}
+	
+	
+	
+	public String getSubnetInput()
+	{
+		return subnetTextFld.getText();
+	}
+	
+	
+	
+	public String getGatewayInput()
+	{
+		return gatewayTextFld.getText();
+	}
+	
+	
+	
+	public String getDnsInput()
+	{
+		return dnsTextFld.getText();
+	}
+	
+	
+	
+	public void setIpInput(final String input)
+	{
+		if (input != null && !input.isEmpty())
+		{
+			ipTextFld.setText(input);
+		}
+	}
+	
+	
+	
+	public void setSubnetInput(final String input)
+	{
+		if (input != null && !input.isEmpty())
+		{
+			subnetTextFld.setText(input);
+		}
+	}
+	
+	
+	
+	public void setGatewayInput(final String input)
+	{
+		if (input != null && !input.isEmpty())
+		{
+			gatewayTextFld.setText(input);
+		}
+	}
+	
+	
+	
+	public void setDnsInput(final String input)
+	{
+		if (input != null && !input.isEmpty())
+		{
+			dnsTextFld.setText(input);
+		}
+	}
 }
